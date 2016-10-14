3d36952be9490817f9a2f6d5901900ed16cb889b
Anudle <AnuShrestha007@gmail.com>
2016-10-14 15:17:32 -0600

    up boats and time works

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-var app = angular.module('RedditApp', []).controller('redditController', function($scope){
-  $scope.post= [];
-  $scope.newPost={}
-  $scope.submitPost = function() {
-  $scope.post.push({title:$scope.newPost.title,
-    author:$scope.newPost.author, description:$scope.newPost.description,
-    image: $scope.newPost.image})
-  $scope.newPost.title='';
-  $scope.newPost.author='';
-  $scope.newPost.image='';
-  $scope.newpost.description;
+var app = angular.module('RedditApp', ['angularMoment']).controller('redditController', function($scope, moment) {
+    $scope.post = [];
+    $scope.newPost = {}
+    $scope.submitPost = function() {
+        $scope.post.push({
+            title: $scope.newPost.title,
+            author: $scope.newPost.author,
+            description: $scope.newPost.description,
+            image: $scope.newPost.image,
+            posted: Date.now(),
+            votes: $scope.newPost.votes = 0
+        })
+        $scope.newPost.title = '';
+        $scope.newPost.author = '';
+        $scope.newPost.image = '';
+        $scope.newPost.description = '';
+
     }
-  })
+    $scope.upVote = function(p) {
+    p.votes += 1;
+  }
+    $scope.downVote = function(p){
+      p.votes -=1;
+    }
+
+
+})
