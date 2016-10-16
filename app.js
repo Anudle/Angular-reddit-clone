f87484862dd4477bb733180728a6bbae1e89c2ea
Anudle <AnuShrestha007@gmail.com>
2016-10-16 13:21:10 -0600

    added sort and add page toggle feature

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var app = angular.module('RedditApp', ['angularMoment']).controller('redditController', function($scope, moment) {
-    $scope.post = [];
-    $scope.newPost = {}
+    $scope.post = [],
+    $scope.newPost = {},
+    $scope.showForm = false
+    $scope.a = function(){
+      $scope.showForm = !$scope.showForm
+    }
+
     $scope.submitPost = function() {
         $scope.post.push({
             title: $scope.newPost.title,
@@ -15,13 +20,12 @@ var app = angular.module('RedditApp', ['angularMoment']).controller('redditContr
         $scope.newPost.image = '';
         $scope.newPost.description = '';
 
-    }
+    },
     $scope.upVote = function(p) {
     p.votes += 1;
-  }
+  },
     $scope.downVote = function(p){
       p.votes -=1;
     }
-
 
 })
