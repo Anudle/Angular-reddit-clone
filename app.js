cd16359b547b5ab7168a9d4d80cd087bc61cd80b
Anudle <AnuShrestha007@gmail.com>
2016-10-17 13:42:03 -0600

    done for code review

--- app.js
+++ app.js
@@ -1,94 +1,93 @@
 var app = angular.module('RedditApp', ['angularMoment']).controller('redditController', function($scope, moment) {
 
 
-    $scope.post = [
-      {
-      title: 'You guys asked: me standing in my mail order bride costume',
-      author: 'Manda-lee23',
-      description: 'Direct trade listicle iPhone small batch shabby chic ethical hoodie pork belly kinfolk, meditation bitters 8-bit quinoa. Shoreditch mixtape kickstarter mumblecore. Roof party seitan pug, irony bushwick ugh blue bottle listicle bespoke green juice selvage franzen cliche. Irony banh mi waistcoat etsy, farm-to-table godard vice chambray skateboard lo-fi affogato.',
-      image:'https://i.reddituploads.com/5d1072ee639f45f6a3ca5ad4d7749b10?fit=max&h=1536&w=1536&s=46f761180c4a1e2cbf3d928d50f8c2b2',
-      posted: 1476609650000,
-      votes: 8,
-      comments:[{
-        author: 'Steve',
-        comment: 'Ah, the kitchen corner by the sink -- most popular spot in parties.'},
-        { author: 'Sam',
-        comment:  'OP delivered.'}
-      ]
-    },
-    {
-    title: 'Qizai, the only brown panda in the world!',
-    author: 'panzerinthehood',
-    description: 'Direct trade listicle iPhone small batch shabby chic ethical hoodie pork belly kinfolk, meditation bitters 8-bit quinoa. Shoreditch mixtape kickstarter mumblecore. Roof party seitan pug, irony bushwick ugh blue bottle listicle bespoke green juice selvage franzen cliche. Irony banh mi waistcoat etsy, farm-to-table godard vice chambray skateboard lo-fi affogato.',
-    image:'https://i.redd.it/hvzv42t0lvrx.jpg',
-    posted: 705489650000,
-    votes: 5,
-    comments:[{
-      author: 'Ji',
-      comment: 'If you accidentally wash a black panda with a load of whites, this is what happens.'},
-      { author: 'Grey',
-      comment:  "He's a shiny Pokémon."}
-    ]
-  },
-  {
-  title: 'Made a tiny home for my wild bird friends.',
-  author: 'jadafitch',
-  description: 'Direct trade listicle iPhone small batch shabby chic ethical hoodie pork belly kinfolk, meditation bitters 8-bit quinoa. Shoreditch mixtape kickstarter mumblecore. Roof party seitan pug, irony bushwick ugh blue bottle listicle bespoke green juice selvage franzen cliche. Irony banh mi waistcoat etsy, farm-to-table godard vice chambray skateboard lo-fi affogato.',
-  image:'https://i.redd.it/vb8p2s25pvrx.jpg',
-  posted: 1476699354000,
-  votes: 1,
-  comments:[{
-    author: 'Navid',
-    comment: 'ok this shit is hella cute also i love the patrick star bird.'},
-    { author: 'Danny',
-    comment:  "That or THERE IS A GIANT BIRD AT YOUR FRONT DOOR"}
-  ]
-},
-    ],
+    $scope.post = [{
+            title: 'You guys asked: me standing in my mail order bride costume',
+            author: 'Manda-lee23',
+            description: 'Direct trade listicle iPhone small batch shabby chic ethical hoodie pork belly kinfolk, meditation bitters 8-bit quinoa. Shoreditch mixtape kickstarter mumblecore. Roof party seitan pug, irony bushwick ugh blue bottle listicle bespoke green juice selvage franzen cliche. Irony banh mi waistcoat etsy, farm-to-table godard vice chambray skateboard lo-fi affogato.',
+            image: 'https://i.reddituploads.com/5d1072ee639f45f6a3ca5ad4d7749b10?fit=max&h=1536&w=1536&s=46f761180c4a1e2cbf3d928d50f8c2b2',
+            posted: 1476609650000,
+            votes: 8,
+            comments: [{
+                author: 'Ryan',
+                comment: 'Ah, the kitchen corner by the sink -- most popular spot in parties.'
+            }, {
+                author: 'Lane',
+                comment: 'OP delivered.'
+            }]
+        }, {
+            title: 'Qizai, the only brown panda in the world!',
+            author: 'panzerinthehood',
+            description: 'Direct trade listicle iPhone small batch shabby chic ethical hoodie pork belly kinfolk, meditation bitters 8-bit quinoa. Shoreditch mixtape kickstarter mumblecore. Roof party seitan pug, irony bushwick ugh blue bottle listicle bespoke green juice selvage franzen cliche. Irony banh mi waistcoat etsy, farm-to-table godard vice chambray skateboard lo-fi affogato.',
+            image: 'https://i.redd.it/hvzv42t0lvrx.jpg',
+            posted: 705489650000,
+            votes: 5,
+            comments: [{
+                author: 'Chey',
+                comment: 'If you accidentally wash a black panda with a load of whites, this is what happens.'
+            }, {
+                author: 'Tom',
+                comment: "He's a shiny Pokémon."
+            }]
+        }, {
+            title: 'Made a tiny home for my wild bird friends.',
+            author: 'jadafitch',
+            description: 'Direct trade listicle iPhone small batch shabby chic ethical hoodie pork belly kinfolk, meditation bitters 8-bit quinoa. Shoreditch mixtape kickstarter mumblecore. Roof party seitan pug, irony bushwick ugh blue bottle listicle bespoke green juice selvage franzen cliche. Irony banh mi waistcoat etsy, farm-to-table godard vice chambray skateboard lo-fi affogato.',
+            image: 'https://i.redd.it/vb8p2s25pvrx.jpg',
+            posted: 1476699354000,
+            votes: 1,
+            comments: [{
+                author: 'Os',
+                comment: 'ok this shit is hella cute also i love the patrick star bird.'
+            }, {
+                author: 'Danny',
+                comment: "That or THERE IS A GIANT BIRD AT YOUR FRONT DOOR"
+            }]
+        }, ],
 
-    $scope.newPost = {},
+        $scope.newPost = {},
 
-    $scope.showForm = false,
-    $scope.showComment= false,
-    $scope.addComment=false,
+        $scope.showForm = false,
+        $scope.showComment = false,
+        $scope.addComment = false,
 
-    $scope.toggleComment = function(){
-      $scope.showComment = !$scope.showComment
+        $scope.toggleComment = function() {
+            $scope.showComment = !$scope.showComment
+        }
+
+    $scope.toggleAddComment = function() {
+        $scope.addComment = !$scope.addComment
     }
 
-    $scope.toggleAddComment = function(){
-      $scope.addComment = !$scope.addComment
-    }
-
-    $scope.toggleAdd = function(){
-      $scope.showForm = !$scope.showForm
+    $scope.toggleAdd = function() {
+        $scope.showForm = !$scope.showForm
     }
 
     $scope.submitPost = function() {
-        $scope.post.push({
-            title: $scope.newPost.title,
-            author: $scope.newPost.author,
-            description: $scope.newPost.description,
-            image: $scope.newPost.image,
-            posted: Date.now(),
-            votes: $scope.newPost.votes = 0,
-            comments: []
-        })
-        $scope.newPost.title = '';
-        $scope.newPost.author = '';
-        $scope.newPost.image = '';
-        $scope.newPost.description = '';
-        $scope.newPost.comments = '';
-        $scope.toggleAdd();
-    },
+            $scope.post.push({
+                title: $scope.newPost.title,
+                author: $scope.newPost.author,
+                description: $scope.newPost.description,
+                image: $scope.newPost.image,
+                posted: Date.now(),
+                votes: $scope.newPost.votes = 0,
+                comments: []
+            })
+            $scope.newPost.title = '';
+            $scope.newPost.author = '';
+            $scope.newPost.image = '';
+            $scope.newPost.description = '';
+            $scope.newPost.comments = '';
+            $scope.toggleAdd();
+        },
 
 
 
-    $scope.upVote = function(p) {
-    p.votes += 1;
-  },
-    $scope.downVote = function(p){
-      p.votes -=1;
-    }
+        $scope.upVote = function(p) {
+            p.votes += 1;
+        },
+        $scope.downVote = function(p) {
+            p.votes -= 1;
+        }
 
 })
